000efacb5fa0326a1617f84de66fc0fc65344d8c
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-04-30 14:44:50 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const id = document.getElementById('input')
+const div = document.getElementById('div')
+let arr = []
+// const input = document.getElementById('')
+
+function onChanged() {
+    console.log('e', id.value)
+}
+
+function onChangedd() {
+    console.log('ee', id.value)
+    console.log('id.value[0]', id.value[id.value.toString().length - 1])
+    // console.log('id.value[0]', id.value.toString().length)
+
+    if (id.value[id.value.toString().length - 1] === ' ') {
+        let word = id.value.trim()
+        arr.push(...[word])
+        id.value = ''
+        console.log('arr', arr)
+        // for (var a = 0; a <= arr.length - 1; a++) {
+        //     div.innerHTML += `${arr[a]}+'/n'`
+        // }
+
+        // div.innerHTML = <
+    }
+}
